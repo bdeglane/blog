ff2322fb490a3cdfb69ea6a3dd23cb9e3f415460
bdeglane <bdeglane@users.noreply.github.com>
2019-10-08 15:48:29 +0200

    :rotating_light: Add eslint

--- packages/front/src/components/layout.jsx
+++ packages/front/src/components/layout.jsx
@@ -5,14 +5,14 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react'
+import PropTypes from 'prop-types'
+import { useStaticQuery, graphql } from 'gatsby'
 
-import Header from "./header"
-import "./layout.css"
+import { Header } from './header'
+import './layout.css'
 
-const Layout = ({ children }) => {
+export function Layout({ children }) {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -28,16 +28,19 @@ const Layout = ({ children }) => {
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
-          margin: `0 auto`,
+          margin: '0 auto',
           maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
+          padding: '0px 1.0875rem 1.45rem',
           paddingTop: 0,
         }}
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
+          ©
+          {' '}
+          {new Date().getFullYear()}
+          , Built with
+
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
@@ -48,5 +51,3 @@ const Layout = ({ children }) => {
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
-
-export default Layout
