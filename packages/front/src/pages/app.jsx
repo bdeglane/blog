5c11e0308fc36a33ee037e61a0d18989782179b1
bdeglane <bdeglane@users.noreply.github.com>
2019-10-10 17:11:43 +0200

    :sparkles: Add react helmet

--- packages/front/src/pages/app.jsx
+++ packages/front/src/pages/app.jsx
@@ -1,8 +1,10 @@
 import React, { Fragment } from 'react'
+import { Head } from '../components/head'
 
 export default function () {
   return (
     <Fragment>
+      <Head />
       <h1>SPA ROUTER</h1>
     </Fragment>
   )
