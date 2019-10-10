332602cb1ef39144b1723ac5c2e6b8f780a716c3
bdeglane <bdeglane@users.noreply.github.com>
2019-10-10 16:42:10 +0200

    :rotating_light: add rules for fragments

--- packages/front/src/components/layout.jsx
+++ packages/front/src/components/layout.jsx
@@ -5,7 +5,7 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { useStaticQuery, graphql } from 'gatsby'
 
@@ -24,7 +24,7 @@ export function Layout({ children }) {
   `)
 
   return (
-    <>
+    <Fragment>
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
@@ -44,7 +44,7 @@ export function Layout({ children }) {
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
-    </>
+    </Fragment>
   )
 }
 
---
+++ packages/front/src/pages/app.jsx
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react'
+
+export default function () {
+  return (
+    <Fragment>
+      <h1>SPA ROUTER</h1>
+    </Fragment>
+  )
+}
